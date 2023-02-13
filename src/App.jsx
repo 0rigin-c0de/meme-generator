5fc78da205fc635841001145a3a790a49bf0d90a
0rigin-c0de <shunnu807@gmail.com>
2023-02-13 21:26:02 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Meme from "./components/Meme.jsx";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
